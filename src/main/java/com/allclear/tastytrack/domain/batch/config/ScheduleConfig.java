a21430481ff64130bb90a7206bc3431cce89882c
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-09-21 21:01:36 +0900

    [Refactor] 배치 작업 실행 방식 변경 (API 호출 -> 스케줄링 방식)

--- src/main/java/com/allclear/tastytrack/domain/batch/config/ScheduleConfig.java
+++ src/main/java/com/allclear/tastytrack/domain/batch/config/ScheduleConfig.java
@@ -2,30 +2,35 @@ package com.allclear.tastytrack.domain.batch.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
-public class ScheduleConfig {
+public class ScheduleConfig { // 배치 작업 스케줄링
 
-    // TODO: 배치 API 호출 방식을 추후 스케줄링 방식으로 전환할 예정입니다.
-//    private final JobLauncher jobLauncher;
-//    private final JobRegistry jobRegistry;
-//
-//    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul") // 한국 시간 기준으로 매일 자정에 실행
-//    public void runFirstJob() throws Exception {
-//
-//        log.info("first schedule start");
-//
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss");
-//        String date = dateFormat.format(new Date());
-//
-//        JobParameters jobParameters = new JobParametersBuilder()
-//                .addString("date", date)
-//                .toJobParameters();
-//
-//        jobLauncher.run(jobRegistry.getJob("firstJob"), jobParameters);
-//    }
+    private final JobLauncher jobLauncher;
+    private final JobRegistry jobRegistry;
+
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul") // 한국 시간 기준으로 매일 자정에 실행
+    public void runFirstJob() throws Exception {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss");
+        String date = dateFormat.format(new Date());
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("date", date)
+                .toJobParameters();
+
+        jobLauncher.run(jobRegistry.getJob("fetchJob"), jobParameters);
+    }
 
 }
